933ff0165bd9cd8e1e6ff09e8a13e13f7cf40fd5
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-03 23:00:55 +0100

    get_code: added support to retrieve contract source code from snowtrace.io

--- get_code.py
+++ get_code.py
@@ -4,26 +4,50 @@ import os
 from argparse import ArgumentParser
 import json
 from typing import List, Tuple
+from dotenv import load_dotenv
+import requests
 from bscscan import BscScan
 
-API_KEY="hardcoded is bad"
-API_KEY=os.getenv("BSCSCAN_API_KEY", API_KEY)
-
-def get_contract_source_code(contract_address: str) -> List[Tuple]:
-    bsc = BscScan(API_KEY)
-    ret = []
-    result = bsc.get_contract_source_code(contract_address)
-    for ee in result:
-        # I don't know why bscscan returned value with 2 { }
-        raw_data = ee.get('SourceCode', '{{}}')
-        if raw_data[0] == '{':
-            data = json.loads(raw_data[1:-1])
-            sources = data.get('sources', {})
+BSC="bsc"
+FANTOM="ftm"
+class Snowtrace(object):
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+    
+    def get_contract_source_code(self, address) -> List[Tuple]:
+        ret = []
+        url = f'https://api.snowtrace.io/api?module=contract&action=getsourcecode&address={address}&apikey={self.api_key}'
+        resp = requests.get(url)
+        if not resp.ok:
+            return []
+        result = resp.json()['result']
+        for ee in result:
+            raw_data = ee.get('SourceCode', '{}')
+            sources = json.loads(raw_data)
             for kk in sources:
                 ret.append((kk, sources[kk]['content']))
-        else:
-            ret.append(("contract.sol", raw_data))
-    return ret
+        return ret
+
+class Binance(object):
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.bsc = BscScan(api_key)
+    
+    def get_contract_source_code(self, address) -> List[Tuple]:
+        ret = []
+        result = self.bsc.get_contract_source_code(address)
+        for ee in result:
+            # I don't know why bscscan returned value with 2 { }
+            raw_data = ee.get('SourceCode', '{{}}')
+            if raw_data[0] == '{':
+                data = json.loads(raw_data[1:-1])
+                sources = data.get('sources', {})
+                for kk in sources:
+                    ret.append((kk, sources[kk]['content']))
+            else:
+                ret.append(("contract.sol", raw_data))
+        return ret
+
 
 def write_contract_source_code(contract_source_code: List[Tuple], output_directory: str) -> int:
     count = 0
@@ -50,13 +74,24 @@ def write_contract_source_code(contract_source_code: List[Tuple], output_directo
         count += 1
     return count
 
+def get_explorer(blockchain: str):
+    if blockchain == BSC:
+        api_key = os.getenv('BSCSCAN_API_KEY')
+        return Binance(api_key)
+    if blockchain == FANTOM:
+        api_key = os.getenv('SNOWTRACE_API_KEY')
+        return Snowtrace(api_key)
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
     parser = ArgumentParser()
     parser.add_argument("--contract", help="Contract address", required=True)
     parser.add_argument("--output", help="Output directory", required=True)
+    parser.add_argument("--blockchain", default=BSC, nargs="?", choices=[BSC, FANTOM])
     args = parser.parse_args()
-    contract_codes = get_contract_source_code(args.contract)
+    explorer = get_explorer(args.blockchain)
+    contract_codes = explorer.get_contract_source_code(args.contract)
     #print(contract_codes)
     ret = write_contract_source_code(contract_codes, args.output)
     print(f"{ret} contract files are saved")
